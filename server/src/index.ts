4c883a244d0c782599aca62b15c6a7295d500b95
Harshit Daga <harshitdaga2003@gmail.com>
2024-11-20 00:12:27 +0530

    CORS Fix - 1

--- server/src/index.ts
+++ server/src/index.ts
@@ -25,7 +25,13 @@ app.use(express.urlencoded({ extended: true }));
       }),
 );*/
 
-app.use(cors());
+//app.use(cors());
+
+app.use(cors({
+      origin: "*", // Allow both origins
+      credentials:true,            //access-control-allow-credentials:true
+      optionSuccessStatus:200
+  }));
 
 app.use(express.json());
 app.use(cookieParser());
