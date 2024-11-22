fdc28b141363d4d58353e689f8f1b7f35946a25a
Manas <mycodeunq@gmail.com>
2024-11-22 16:51:47 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import Logo from '../assets/VivaL.webp';
+import Logo from '../assets/vivalogo.png';
 import { useNavigate } from 'react-router-dom';
 import { useRef } from 'react';
 import './button.css';
@@ -11,20 +11,38 @@ const Navbar = () => {
             <div className="flex items-center justify-center w-[11vw] h-[11vw] lg:w-[11vh] lg:h-[11vh]">
                 <img src={Logo} alt="Logo" className=" h-[8vw] lg:h-[8vh]" />
             </div>
+
+            <div className='gap-5 hidden sm:flex'>
+
             <button
                 onClick={e => navigate('/caportal')}
-                className="flex text-white dark:text-black group relative cursor-pointer overflow-hidden whitespace-nowrap h-11 px-6  [background:var(--bg)] [border-radius:var(--radius)] transition-all shadow-[0_0_0_3px_rgba(255,255,255,0.3)_inset] hover:scale-105 duration-300  w-max  items-center justify-center  hover:shadow-[0_0_0_3px_rgba(255,255,255,0.3)_inset]"
-            >
+                className="flex text-black dark:text-black group relative cursor-pointer overflow-hidden whitespace-nowrap h-11 px-6 transition-all shadow-[0_0_0_3px_rgba(255,255,255,0.3)_inset] hover:scale-105 duration-300 w-max items-center justify-center bg-gradient-to-r from-white via-purple-300 to-purple-500 hover:bg-gradient-to-r hover:from-purple-600 hover:to-black hover:text-white [border-radius:var(--radius)]"
+                >
                 <div className="absolute inset-0 overflow-visible [container-type:size]">
-                    <div className="absolute inset-0 h-[100cqh] animate-slide [aspect-ratio:1] [border-radius:0] [mask:none] ">
+                    <div className="absolute inset-0 h-[100cqh] animate-slide [aspect-ratio:1] [border-radius:0] [mask:none] group-hover:opacity-0">
                         <div className="absolute inset-[-100%] w-auto rotate-0 animate-spin [background:conic-gradient(from_calc(270deg-(var(--spread)*0.5)),transparent_0,hsl(0_0%_100%/1)_var(--spread),transparent_var(--spread))] [translate:0_0]"></div>
                     </div>
                 </div>
                 <div className="absolute [background:var(--bg)] [border-radius:var(--radius)] [inset:var(--cut)]"></div>
-                <span className="relative whitespace-pre text-center text-base font-semibold leading-none tracking-tight text-white z-10 font-mabry">
+                <span className="relative whitespace-pre text-center text-base font-semibold leading-none tracking-tight  z-10 font-mabry">
+                    Event Brochure
+                </span>
+            </button>
+            <button
+                onClick={e => navigate('/caportal')}
+                className="flex text-black dark:text-black group relative cursor-pointer overflow-hidden whitespace-nowrap h-11 px-6 transition-all shadow-[0_0_0_3px_rgba(255,255,255,0.3)_inset] hover:scale-105 duration-300 w-max items-center justify-center bg-gradient-to-r from-white via-purple-300 to-purple-500 hover:bg-gradient-to-r hover:from-purple-600 hover:to-black hover:text-white [border-radius:var(--radius)]"
+                >
+                <div className="absolute inset-0 overflow-visible [container-type:size]">
+                    <div className="absolute inset-0 h-[100cqh] animate-slide [aspect-ratio:1] [border-radius:0] [mask:none] group-hover:opacity-0">
+                        <div className="absolute inset-[-100%] w-auto rotate-0 animate-spin [background:conic-gradient(from_calc(270deg-(var(--spread)*0.5)),transparent_0,hsl(0_0%_100%/1)_var(--spread),transparent_var(--spread))] [translate:0_0]"></div>
+                    </div>
+                </div>
+                <div className="absolute [background:var(--bg)] [border-radius:var(--radius)] [inset:var(--cut)]"></div>
+                <span className="relative whitespace-pre text-center text-base font-semibold leading-none tracking-tight  z-10 font-mabry">
                     CA Portal
                 </span>
             </button>
+                </div>
         </header>
     );
 };
