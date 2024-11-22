0636adff5ce27df7dcca41d66e014071586cd5db
Harshit Daga <harshitdaga2003@gmail.com>
2024-11-23 01:14:47 +0530

    CORS Fix 6

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,7 +20,7 @@ app.use(
                   'https://vivacity-2025-campus-ambassador.vercel.app',
                   'http://localhost:5173',
             ], // Allow both origins
-            credentials: true, // access-control-allow-credentials:true
+            //credentials: true, // access-control-allow-credentials:true
             optionsSuccessStatus: 200,
             methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Add allowed methods
       }),
