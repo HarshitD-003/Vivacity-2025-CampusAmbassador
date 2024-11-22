6215aabceb8d8d56e210f01610342cf879e3881c
Manas <mycodeunq@gmail.com>
2024-11-22 15:17:57 +0530

    Update NormNav.jsx

--- client/src/components/NormalRegistration/NormNav.jsx
+++ client/src/components/NormalRegistration/NormNav.jsx
@@ -1,9 +1,9 @@
-import V24Logo from '../../assets/viva24Logo.svg';
+import V24Logo from '../../assets/viva_logo_25.png';
 
 const NormNav = () => {
     return (
         <header className=" w-full py-12 lg:py-4 flex justify-center items-center">
-            <img src={V24Logo} className=" h-[12vh] " />
+            <img src={V24Logo} className=" h-[18vh] " />
         </header>
     );
 };
