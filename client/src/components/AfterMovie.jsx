acd50cebc60668320c0ab25d2fa5bb4b4cef8bdd
Harshit Daga <harshitdaga2003@gmail.com>
2024-11-30 18:47:48 +0530

    AfterMovie Updated

--- client/src/components/AfterMovie.jsx
+++ client/src/components/AfterMovie.jsx
@@ -3,7 +3,7 @@ const AfterMovie = () => {
         <div className="md:h-[30vh] md:mt-20 h-[24vh] snap-start w-full">
             <iframe
                 className=" h-full w-full"
-                src="https://www.youtube.com/embed/E64TKUqgi_4?si=tHHQHdYJ_8XpZ2sr"
+                src="https://www.youtube.com/embed/MNJ8tqQgmnw?si=KDc9wJuQ1SPYt1IN"
                 title="YouTube video player"
                 frameBorder="0"
                 // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
